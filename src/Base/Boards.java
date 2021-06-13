e6980f877566f1df73b0b11530c3c6135478a883
alexmason <8usernameunknown8@gmail.com>
2021-06-13 09:21:05 +0300

    update

---
+++ src/Base/Boards.java
@@ -0,0 +1,150 @@
+package Base;
+
+public class Boards {
+
+    public static final String B = "b";
+    public static final String NEWS = "news";
+    public static final String VG = "vg";
+    public static final String PO = "po";
+    public static final String FAG = "fag";
+    public static final String SEX = "sex";
+    public static final String V = "v";
+    public static final String GG = "g";
+    public static final String HW = "hw";
+    public static final String SOC = "soc";
+    public static final String AU = "au";
+    public static final String MOV = "mov";
+    public static final String SP = "sp";
+    public static final String D = "d";
+    public static final String A = "a";
+    public static final String FTB = "ftb";
+    public static final String MOBI = "mobi";
+    public static final String PR = "pr";
+    public static final String WRK = "wrk";
+    public static final String CG = "cg";
+    public static final String ME = "me";
+    public static final String MA = "ma";
+    public static final String CC = "cc";
+    public static final String PSY = "psy";
+    public static final String BI = "bi";
+    public static final String _2D = "2d";
+    public static final String FET = "fet";
+    public static final String FIZ = "fiz";
+    public static final String FG = "fg";
+    public static final String FA = "fa";
+    public static final String TV = "tv";
+    public static final String T = "t";
+    public static final String E = "e";
+    public static final String TES = "tes";
+    public static final String HC = "hc";
+    public static final String HRY = "hry";
+    public static final String GSG = "gsg";
+    public static final String JA = "ja";
+    public static final String BIZ = "biz";
+    public static final String SN = "sn";
+    public static final String FD = "fd";
+    public static final String DIY = "diy";
+    public static final String BO = "bo";
+    public static final String MU = "mu";
+    public static final String EM = "em";
+    public static final String MUS = "mus";
+    public static final String ZOG = "zog";
+    public static final String BY = "by";
+    public static final String PA = "pa";
+    public static final String W = "w";
+    public static final String DR = "dr";
+    public static final String SPC = "spc";
+    public static final String UN = "un";
+    public static final String MG = "mg";
+    public static final String H = "h";
+    public static final String C = "c";
+    public static final String FS = "fs";
+    public static final String P = "p";
+    public static final String HI = "hi";
+    public static final String FL = "fl";
+    public static final String WH = "wh";
+    public static final String RE = "re";
+    public static final String BG = "bg";
+    public static final String DI = "di";
+    public static final String GD = "gd";
+    public static final String RA = "ra";
+    public static final String ALCO = "alco";
+    public static final String MO = "mo";
+    public static final String BRG = "brg";
+    public static final String ES = "es";
+    public static final String VAPE = "vape";
+    public static final String CH = "ch";
+    public static final String TD = "td";
+    public static final String UKR = "ukr";
+    public static final String SCI = "sci";
+    public static final String MMO = "mmo";
+    public static final String VN = "vn";
+    public static final String SF = "sf";
+    public static final String ASMR = "asmr";
+    public static final String IZD = "izd";
+    public static final String DOM = "dom";
+    public static final String SOCIONICS = "socionics";
+    public static final String MC = "mc";
+    public static final String FUR = "fur";
+    public static final String WOW = "wow";
+    public static final String MLP = "mlp";
+    public static final String KPOP = "kpop";
+    public static final String INT = "int";
+    public static final String R = "r";
+    public static final String SE = "se";
+    public static final String PH = "ph";
+    public static final String LAW = "law";
+    public static final String NE = "ne";
+    public static final String DE = "de";
+    public static final String WR = "wr";
+    public static final String WHN = "whn";
+    public static final String HH = "hh";
+    public static final String O = "o";
+    public static final String QTR4 = "qtr4";
+    public static final String KZ = "kz";
+    public static final String WP = "wp";
+    public static final String M = "m";
+    public static final String HO = "ho";
+    public static final String GABE = "gabe";
+    public static final String MEDIA = "media";
+    public static final String RUVN = "ruvn";
+    public static final String OUT = "out";
+    public static final String HV = "hv";
+    public static final String MATH = "math";
+    public static final String TRV = "trv";
+    public static final String LD = "ld";
+    public static final String PVC = "pvc";
+    public static final String OBR = "obr";
+    public static final String SW = "sw";
+    public static final String WEB = "web";
+    public static final String USSR = "ussr";
+    public static final String GURO = "guro";
+    public static final String IND = "ind";
+    public static final String TR = "tr";
+    public static final String SAD = "sad";
+    public static final String VR = "vr";
+    public static final String WWE = "wwe";
+    public static final String POK = "pok";
+    public static final String BR = "br";
+    public static final String OLD = "old";
+    public static final String EW = "ew";
+
+
+    private static void main(String[] args) {
+        test();
+    }
+
+    private static void test() {
+        int all = 0;
+        int success = 0;
+        int fail = 0;
+
+        for (int i = 0; i < all; i++) {
+
+        }
+
+
+    }
+
+
+}
